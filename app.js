ab512d8bc953e88e3ef6858f304161bcad251c7e
Seun Fadeyi <seunfadeyi@gmail.com>
2020-04-06 21:18:27 +0100

    bugs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
+require('dotenv').config();
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
